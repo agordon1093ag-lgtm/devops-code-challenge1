99fe4f7adff685f9ca0924f4aba9fe25c0198222
Aundrea Gordon <aundreagordon@Aundreas-Mac-mini.local>
2026-02-17 17:27:37 -0600

    Add trailing slash to backend URL

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,4 @@
 const config = {
-  backendUrl: "http://devops-challenge-alb-1281915073.us-east-2.elb.amazonaws.com/api"
+  backendUrl: "http://devops-challenge-alb-1281915073.us-east-2.elb.amazonaws.com/api/"
 };
 export default config;
